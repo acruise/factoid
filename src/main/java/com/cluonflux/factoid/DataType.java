8483d5958240557db2a3a8e2c4db5bf2687482a2
Alex Cruise <alex@metaforsoftware.com>
2013-05-31 16:50:56 -0700

    DataType is now a Java enum; some docs.

---
+++ src/main/java/com/cluonflux/factoid/DataType.java
@@ -0,0 +1,12 @@
+package com.cluonflux.factoid;
+
+/**
+ * @author Alex Cruise <alex@metaforsoftware.com>
+ */
+public enum DataType {
+    String,
+    Integer,
+    FloatingPoint,
+    Enumerated,
+    Json
+}
